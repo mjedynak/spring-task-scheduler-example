3abc9ee0bda8782ba5503e0a303243bbdec962eb
Michal Jedynak <m.jedynak@gmail.com>
2014-02-26 20:24:04 +0100

    formatting

--- src/main/java/pl/mjedynak/App.java
+++ src/main/java/pl/mjedynak/App.java
@@ -2,10 +2,8 @@ package pl.mjedynak;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class App {
     public static void main(String[] args) {
         new AnnotationConfigApplicationContext(AppConfig.class);
     }
-
 }
